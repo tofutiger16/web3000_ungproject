5e25b2b9ca33f5fad686efb7f58651a213c1cbaf
tofutiger16 <t.hitt2014@gmail.com>
2021-04-20 15:12:08 -0400

    Checking off wishlist stuff

--- thecrypt.js
+++ thecrypt.js
@@ -17,6 +17,8 @@
       document.getElementById('mode').disabled = true;
       document.getElementById('cipher-padding').value = "";
       document.getElementById('cipher-padding').disabled = true;
+	  document.getElementById('rsa-key-row').style.display = "none";
+	  $("select option[value='pkcs1']").prop('disabled', true);
     }
     else if (inputMethod === "rsa") {
       document.getElementById("passphrase").placeholder="";
@@ -24,12 +26,15 @@
       document.getElementById("passphrase").disabled = true;
       document.getElementById('mode').value = "";
       document.getElementById('mode').disabled = true;
-      document.getElementById('cipher-padding').value = "pkcs7";
+      document.getElementById('cipher-padding').value = "pkcs1";
       document.getElementById('cipher-padding').disabled = true;
+	  document.getElementById('rsa-key-row').style.display = "flex";
     }
 	else if (inputMethod === "des") {
 	  $("select option[value='iso7816']").prop('disabled', false);
 	  $("select option[value='iso97971']").prop('disabled', true);
+	  document.getElementById('rsa-key-row').style.display = "none";
+	  $("select option[value='pkcs1']").prop('disabled', true);
 	}
     else {
       document.getElementById("passphrase").placeholder = "Secret password";
@@ -37,7 +42,9 @@
       document.getElementById('mode').disabled = false;
       document.getElementById('cipher-padding').disabled = false;
 	  $("select option[value='iso7816']").prop('disabled', true);
-	    $("select option[value='iso97971']").prop('disabled', false);
+	  $("select option[value='iso97971']").prop('disabled', false);
+	  document.getElementById('rsa-key-row').style.display = "none";
+	  $("select option[value='pkcs1']").prop('disabled', true);
     }
   }
 
